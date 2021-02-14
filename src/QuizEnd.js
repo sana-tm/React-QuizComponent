d7a7a82b39c73e543146f729e5658c1dbcffe4e9
Sana Mulla <sanashaikh1213@gmail.com>
2021-02-14 22:18:32 +0100

    'Completed_module'

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -1,6 +1,9 @@
 import React, { Component} from 'react'
 
 class QuizEnd extends Component{
+  handleResetClick(){
+    <a onClick='this.handleResetClick.bind(this)'></a>
+  }
   render(){
     return(
       <div>
